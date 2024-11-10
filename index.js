45d2d07e9193f43837827e7c8bfb80985ca478e0
Rahil Singhi <rahilsinghi300@gmail.com>
2024-11-09 21:23:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import card from "./Card";
+import FormField from "./FormField";
+import Loader from "./Loader";
+// In ../components/index.js
+export { default as Loader } from './Loader';
+export { default as Card } from './Card';
+export { default as FormField } from './FormField';
